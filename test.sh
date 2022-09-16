25b3eaae5de6f63e3c8c962563d0031b5dfbe500
tph6hc <fixed-term.Phuong.TranPhamTien@vn.bosch.com>
2022-09-16 16:33:52 +0700

    IO Redirections, task1-shell script

---
+++ test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#echo "What is your name?";
+#name="Peter Phuong Tran";
+#readonly name;
+#echo "Hello, $name";
+#unset name;
+#name="peter";
+
+name="Peter Phuong"
+#echo $?
+#echo $$
+#echo $!
+
+#echo "File Name: $0"
+#echo "First Parameter: $1"
+#echo "Secon Parameter: $2"
+#echo "Quoted Values: $@"
+#echo "Quoted Values: $*"
+#echo "Total Number of Parameters: $#"
+
+#echo $?
+#for TOKEN in $*
+#do
+#	echo $TOKEN
+#done	
+#name=(1 2 3 4 5);
+#echo ${name[0]}
+#echo ${name[1]}
+#echo ${name[2]}
+#echo ${name[3]}
+#echo ${name[4]}
+#echo ${name[*]}
+#echo ${name[@]}
+
+#val=`expr 2 + 2`
+#echo "Total value: $val"
+
+#echo Hello\; Word
+
+#VAR=ZARA
+#echo "$VAR owes <-\$1500.**>; [ as of (`date +%m/%d`) ]"
+
+find -name "hmi_base.sh"
+echo $?
