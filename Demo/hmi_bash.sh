cc452fbca41e768eb94a54141259c3d97ad6a5ff
tph6hc <fixed-term.Phuong.TranPhamTien@vn.bosch.com>
2022-09-21 10:48:34 +0700

    2nd Version of task1 & task2

---
+++ Demo/hmi_bash.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo "hmi_bash.sh have just been executed"
--- task1.sh
+++ task1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-find_result=`find -name hmi_bash.sh`
+find_result=`find -maxdepth 1 -name hmi_bash.sh`
 if [[ -z $find_result ]]
 then
 	touch hmi_bash.sh
@@ -10,13 +10,13 @@ fi
 echo -e '#!/bin/sh\necho "hmi_bash.sh have just been executed"' > hmi_bash.sh
 chmod 755 hmi_bash.sh
 
-find_demo=`find -name Demo`
+find_demo=`find -maxdepth 1 -name Demo`
 if [[ -z $find_demo ]]
 then
 	mkdir Demo
 fi
 
-find_hello=`find Demo/hello.txt`
+find_hello=`find -maxdepth 1 Demo/hello.txt`
 if [[ -z $find_demo ]]
 then
 	touch Demo/hello.txt
